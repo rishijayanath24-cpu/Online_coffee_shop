82f9188e38dc17657277e93945fafdff8593dc4c
rishijayanath24-cpu <rishijayanath24@gmail.com>
2025-10-31 09:27:34 +0530

    Update AdminDashboard.js

--- frontend/src/components/AdminDashboard.js
+++ frontend/src/components/AdminDashboard.js
@@ -1,3 +1,4 @@
+import API_URL from '../config';
 import React, { useState, useEffect } from 'react';
 import { useAuth } from '../context/AuthContext';
 import { useNavigate } from 'react-router-dom';
