fd398fe2df2439d26e82507828139bc248b3d638
rishijayanath24-cpu <rishijayanath24@gmail.com>
2025-10-31 09:28:02 +0530

    Import API_URL from config in Checkout component

--- frontend/src/components/Checkout.js
+++ frontend/src/components/Checkout.js
@@ -1,3 +1,4 @@
+import API_URL from '../config';
 import React, { useState } from 'react';
 import { Container, Row, Col, Form, Button, Card } from 'react-bootstrap';
 import { useNavigate } from 'react-router-dom';
